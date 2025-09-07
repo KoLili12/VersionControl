b79c99d139d5d81094a42ef504942a9d46f4183f
NickZhirnoff <nickzhirnoff@gmail.com>
2025-09-07 17:59:35 +0300

    Initial Commit

---
+++ VersionControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VersionControl
+//
+//  Created by Николай Жирнов on 07.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
